eac5b0281483ccfe22ec476c01f6bd6e24b7e199
souhail izrhour <souhail.izrhour@gmail.com>
2026-02-28 17:28:54 +0000

    add @name.com to keyboard

--- front/src/Components/Login.jsx
+++ front/src/Components/Login.jsx
@@ -377,7 +377,8 @@ function Login() {
                       "q w e r t y u i o p",
                       "a s d f g h j k l",
                       "z x c v b n m @ .",
-                      "{space} {bksp}",
+                      "@metropole.com",
+                      "{space} {bksp}"
                     ],
                   }}
                   display={{
--- front/src/Modals/VirtualKeyboard.jsx
+++ front/src/Modals/VirtualKeyboard.jsx
@@ -47,8 +47,9 @@ function VirtualKeyboard({ inputName, inputValue, onChange, onKeyPress, onHide }
             "1 2 3 4 5 6 7 8 9 0 .",
             "q w e r t y u i o p",
             "a s d f g h j k l",
-            "z x c v b n m",
-            "{space} {bksp} @ .com",
+            "z x c v b n m @",
+             "@metropole.com",
+            "{space} {bksp} .com",
           ],
         }}
         display={{
