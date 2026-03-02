88c84ee819c506530b00364efef0fcc883432667
souhail izrhour <souhail.izrhour@gmail.com>
2026-03-02 11:53:23 +0000

    add symbole en gras and '

--- front/src/Modals/VirtualKeyboard.jsx
+++ front/src/Modals/VirtualKeyboard.jsx
@@ -55,22 +55,22 @@ function VirtualKeyboard({ inputName, inputValue, onChange, onKeyPress, onHide }
           "q w e r t y u i o p",
           "a s d f g h j k l",
           "z x c v b n m @",
-          "@metropole.com + - .",
-          "{caps} {space} {bksp}",
+          "' + - .",
+          "@metropole.com {caps} {space} {bksp}",
         ],
         caps: [
           "1 2 3 4 5 6 7 8 9 0",
           "Q W E R T Y U I O P",
           "A S D F G H J K L",
           "Z X C V B N M @",
-           "@metropole.com + - .",
-          "{caps} {space} {bksp}",
+          "' + - .",
+          "@metropole.com {caps} {space} {bksp}",
         ]
       }}
       display={{
         "{bksp}": "⌫",
         "{space}": "Espace",
-        "{caps}": "Upper"
+        "{caps}": "⇪",
       }}
       theme={"hg-theme-default hg-layout-default"}
       physicalKeyboardHighlight={false}
